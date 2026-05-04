693eb57206e01c6aaa0b0ef6f948675140afe4b3
Slavic-Squat <jahea98@gmail.com>
2026-05-04 16:09:19 -0500

    - Update.

--- MissileControl/Program.cs
+++ MissileControl/Program.cs
@@ -34,7 +34,7 @@ namespace IngameScript
 
         private static List<IMyTerminalBlock> _allBlocks = new List<IMyTerminalBlock>();
         private const string _programName = "MissileControl";
-        private const string _programVersion = "1.40";
+        private const string _programVersion = "1.41";
 
         private SystemCoordinator _systemCoordinator;
         private bool _isInitialized = false;
